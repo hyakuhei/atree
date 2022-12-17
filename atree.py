4b05768af819c5d72852e269330607ded9598ff3
rob <robert.g.clark@oracle.com>
2022-12-16 16:57:49 -0800

    Added a --output function to create a PNG from the mermaid text using docker

--- atree.py
+++ atree.py
@@ -1,7 +1,6 @@
-import sys
+import sys, tempfile, subprocess, os
 
 if __name__ == "__main__":
-
     map = {'1':'A',
         '2':'B',
         '3':'C',
@@ -41,13 +40,39 @@ if __name__ == "__main__":
         if node_id[:-1] in nodes:
             links.append(f"{node_id[:-1]} --> {node_id}" )
 
-    print("graph TD")
+
+    graph_text = "graph TD\n"
     for node_id in nodes.keys():
-        print(f"\t{node_id}[{nodes[node_id]}]")
+        graph_text += f"\t{node_id}[{nodes[node_id]}]\n"
     
     for link in links:
-        print(f"\t{link}")
-            
+        graph_text += f"\t{link}\n"
+
+    # If no-args just print 
+    if len(sys.argv) == 1:    
+        print(graph_text)
+
+    if len(sys.argv) == 2:
+        if sys.argv[1] in ["-h", "--help", "-help"]:
+            print("Read from STDIN and create a mermaid diagram.")
+            print("Without modification, atree will read in text and print out mermaid text")
+            print("Example: $ cat list.txt | python3 atree.py > tree.mm")
+            print("--out <basename>")
+            print("Example: $ cat list.txt | python3 atree.py --out tree")
+            print("This will create a tree.txt and a tree.png")
+
+    if len(sys.argv) == 3:
+        if "--out" == sys.argv[1]:
+            # Call the mermaid CLI (expecting Docker) to generate the file
+            # 1. Write the tree text
+            # 2. Launch docker, have it create the PNG output
+
+            with open(f"{sys.argv[2]}.mm", "w") as f:
+                f.write(graph_text)
+
+            _ = subprocess.run(
+                ["docker", "run", "--rm", "-u", f"{os.geteuid()}:{os.getegid()}", "-v", f"{os.getcwd()}:/data", "minlag/mermaid-cli", "--input", f"/data/{sys.argv[2]}.mm", "--output", f"/data/{sys.argv[2]}.png"]
+            )
             
 
 
